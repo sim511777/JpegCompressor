c302fe6b4a2df20749c446ea5b0c71c2e262e361
shshim <sim51177@gmail.com>
2019-01-03 14:33:15 +0900

    소스 정리

--- JpegCompressor/FormMain.cs
+++ JpegCompressor/FormMain.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
@@ -16,31 +15,42 @@ namespace JpegCompressor {
          InitializeComponent();
       }
 
-      private static ImageCodecInfo GetEncoderInfo(String mimeType) {
-         return ImageCodecInfo.GetImageEncoders().FirstOrDefault(codecInfo => codecInfo.MimeType == mimeType);
-      }
-
       private void PasteImage() {
-         Image img = Clipboard.GetImage();
+         // 클립보드 로드
+         var img = Clipboard.GetImage();
          if (img == null)
             return;
+
+         // 화면 표시
          this.pbxSrc.DrawingImage = new Bitmap(img);
-         this.lblSrc.Text = $"{this.pbxSrc.DrawingImage.Width*this.pbxSrc.DrawingImage.Height*3/1000}KB";
+
+         // 정보 표시
+         this.lblSrc.Text = $"{img.Width * img.Height * 3 / 1000}KB";
       }
  
       private void CompressImage() {
          if (this.pbxSrc.DrawingImage == null)
             return;
-         this.lblQuality.Text = $"{this.trbQuality.Value}%";
-         ImageCodecInfo codec = GetEncoderInfo("image/jpeg");                       // 코덱선택
-         EncoderParameters encoderParams = new EncoderParameters(1);                // 1개짜리 인코더 파라미터 생성
-         encoderParams.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, this.trbQuality.Value); // 파라미터 값 설정
-         MemoryStream ms = new MemoryStream();
-         this.pbxSrc.DrawingImage.Save(ms, codec, encoderParams);
+
+         // 인코더 준비
+         var enc = ImageCodecInfo.GetImageEncoders().FirstOrDefault(codecInfo => codecInfo.FormatID == ImageFormat.Jpeg.Guid);
+         var encPrms = new EncoderParameters(1);
+         encPrms.Param[0] = new EncoderParameter(Encoder.Quality, this.trbQuality.Value);
+
+         // 메모리 스트림으로 저장
+         var ms = new MemoryStream();
+         this.pbxSrc.DrawingImage.Save(ms, enc, encPrms);
+
+         // 메모리 스트림에서 로드
          Image img = Image.FromStream(ms);
+
+         // 화면 표시
          this.pbxDst.DrawingImage = new Bitmap(img);
          this.pbxDst.Invalidate();
-         this.lblDst.Text = $"{ms.Position/1000}KB";
+
+         // 정보 표시
+         this.lblQuality.Text = $"{this.trbQuality.Value}%";
+         this.lblDst.Text = $"{ms.Position / 1000}KB";
       }
 
       private void btnPaste_Click(object sender, EventArgs e) {
